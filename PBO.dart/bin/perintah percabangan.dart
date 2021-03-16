df245b6a00f2c57ba41af7c6094d58f9afb66104
akniputra <aknibroken212@gmail.com>
2021-03-16 10:52:02 +0700

    percabangan if

--- PBO.dart/bin/perintah percabangan.dart
+++ PBO.dart/bin/perintah percabangan.dart
@@ -1,13 +1,20 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  var number = int.tryParse(stdin.readLineSync());
+  int number = int.tryParse(stdin.readLineSync());
 
-  if (number > 0) {
-    print("positif");
-  } else if (number < 0) {
-    print("negatif");
-  } else {
-    print("nol");
-  }
+  switch (number) {
+    case 0:
+      print("nol");
+      break;
+    case 1:
+      print(number);
+      print("satu");
+      break;
+    case 2:
+      print("dua");
+      break;
+    default:
+      print("bilangan lain" )
+
 }
